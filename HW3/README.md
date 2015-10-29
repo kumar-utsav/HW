c26e8c67c23a2ff5669e513e0b28de03021a2e76
kumar-utsav <kmr.utsav@gmail.com>
2015-10-28 23:47:51 -0400

    Add HW3

---
+++ HW3/README.md
@@ -0,0 +1,23 @@
+# HW #3 Proxies, Queues, Cache Fluency.
+
+### Tasks
+
+#### Complete set/get
+
+
+
+#### Complete recent
+
+
+
+#### Complete upload/meow
+
+
+
+#### Additional service instance running
+
+
+
+#### Demonstrate proxy
+
+
---
+++ HW3/proxy.js
@@ -0,0 +1,108 @@
+var redis = require("redis")
+var request = require("request")
+var express = require("express")
+var bodyParser = require('body-parser')
+var multer = require('multer')
+
+var upload = multer(); 
+var app = express()
+var client = redis.createClient(6379, '127.0.0.1', {})
+
+var next = 3000;
+
+var server = app.listen(3002, function() {
+    var host = server.address().address;
+    var port = server.address().port;
+
+    client.set('prev', 3000);
+
+    console.log('Example app listening at http://%s:%s', host, port);
+
+});
+
+
+// parse application/json
+app.use(bodyParser.json())
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+function resolvePort() {
+
+    client.get('prev', function(err, value) {
+        if (value == 3000) {
+            next = 3001;
+            client.set('prev', 3001);
+        } else {
+            next = 3000;
+            client.set('prev', 3000);
+        }
+    });
+
+    return next;
+}
+
+
+app.get('/', function(req, res) {
+    var port = resolvePort();
+    console.log("Response from server localhost:" + port);
+    request('http://localhost:' + port + '/', function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            res.send(body)
+        }
+    })
+});
+
+app.get('/get', function(req, res) {
+    var port = resolvePort();
+    console.log("Response from server localhost:" + port);
+    request('http://localhost:' + port + '/get', function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            res.send(body)
+        }
+    })
+});
+
+app.get('/set', function(req, res) {
+    var port = resolvePort();
+    console.log("Response from server localhost:" + port);
+    request('http://localhost:' + port + '/set', function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            res.send(body)
+        }
+    })
+});
+
+app.post('/upload', function(req, res) {
+    var port = resolvePort();
+    console.log("Response from server localhost:" + port);
+
+    console.log(req)
+
+    request('http://localhost:' + port + '/upload', function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            res.send(body)
+        }
+    })
+
+});
+
+app.get('/meow', function(req, res) {
+    var port = resolvePort();
+    console.log("Response from server localhost:" + port);
+    request('http://localhost:' + port + '/meow', function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            res.send(body)
+        }
+    })
+});
+
+app.get('/recent', function(req, res) {
+    var port = resolvePort();
+    console.log("Response from server localhost:" + port);
+    request('http://localhost:' + port + '/recent', function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            res.send(body)
+        }
+    })
+});
